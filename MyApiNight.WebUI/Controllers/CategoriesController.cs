a9c7edf4613ec5ce536b4c6d1c46382c52896bed
Abdulhamitkisir <hamitkisi@gmail.com>
2024-12-19 01:41:28 +0100

    28. Ders

--- MyApiNight.WebUI/Controllers/CategoriesController.cs
+++ MyApiNight.WebUI/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyApiNight.WebUI.Dtos;
 using Newtonsoft.Json;
+using System.Text;
 using System.Text.Json.Serialization;
 
 namespace MyApiNight.WebUI.Controllers
@@ -19,10 +20,69 @@ namespace MyApiNight.WebUI.Controllers
             var client=_httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("https://localhost:7032/api/Category");
             if (responseMessage.IsSuccessStatusCode)
-            { 
-                var jsondata=await responseMessage.Content.ReadAsStringAsync();
+            {
+                //DeserializeObject Json datasini Stringe ceviriyor
+                var jsondata =await responseMessage.Content.ReadAsStringAsync();
                 var values=JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsondata);
                 return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateCategory()
+        { 
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            //SerializeObject  text i Json datasina ceviriyor.
+            var client = _httpClientFactory.CreateClient();
+            var jsondata=JsonConvert.SerializeObject(createCategoryDto);
+            StringContent stringContent = new StringContent(jsondata,Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7032/api/Category", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            { 
+                return RedirectToAction("CategoryList");
+            }
+            return View();
+        }
+                
+        public async Task<IActionResult> DeleteCategory(int id) 
+        {
+            var client= _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:7032/api/Category?id=" + id);
+            if (responseMessage.IsSuccessStatusCode) 
+            {
+                return RedirectToAction("CategoryList");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory(int id)
+        { 
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7032/api/Category/GetCategory?id="+id);
+            if (responseMessage.IsSuccessStatusCode) 
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateCategoryDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        { 
+            var client=_httpClientFactory.CreateClient();
+            var jsonData=JsonConvert.SerializeObject(updateCategoryDto);
+            StringContent stringContent=new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7032/api/Category/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("CategoryList");
             }
             return View();
         }
